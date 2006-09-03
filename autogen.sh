c30fc828fab5a793f1dfa03ef2df2a9e5142becc
Brad Fitzpatrick <brad@danga.com>
2006-09-03 23:57:20 +0000

    note for self
    git-svn-id: http://code.sixapart.com/svn/memcached/trunk/server@324 b0b603af-a30f-0410-a34e-baf09ae79d0b

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,10 @@
 #
 # This is hacky, because there are so many damn versions
 # of autoconf/automake.  It works with Debian woody, at least.
-# 
+#
+# Debian sarge:
+#    apt-get install automake1.7 autoconf
+#
 
 echo "aclocal..."
 ACLOCAL=${ACLOCAL:-aclocal-1.7}
