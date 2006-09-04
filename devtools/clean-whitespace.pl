64b5888d5006bc7316c63c2468d4cd0d737cb725
Brad Fitzpatrick <brad@danga.com>
2006-09-04 02:18:42 +0000

    whitespace cleaner
    git-svn-id: http://code.sixapart.com/svn/memcached/trunk/server@327 b0b603af-a30f-0410-a34e-baf09ae79d0b

---
+++ devtools/clean-whitespace.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use FindBin qw($Bin);
+chdir "$Bin/.." or die;
+my @files = (glob("*.h"), glob("*.c"));
+foreach my $f (@files) {
+    open(my $fh, $f) or die;
+    my $before = do { local $/; <$fh>; };
+    close ($fh);
+    my $after = $before;
+    $after =~ s/\t/    /g;
+    $after =~ s/\s+$//m;
+    next if $after eq $before;
+    open(my $fh, ">$f") or die;
+    print $fh $after;
+    close($fh);
+}
