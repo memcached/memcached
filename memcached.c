2a1f2daad30308a89d255f8a0d29ddb4edc42e01
js <ebgssth@gmail.com>
2007-11-14 07:00:04 +0000

    Define IOV_MAX for OS X Panther and earlier. Report submitted by js (ebgssth@gmail.com)
    git-svn-id: http://code.sixapart.com/svn/memcached/trunk/server@631 b0b603af-a30f-0410-a34e-baf09ae79d0b

--- memcached.c
+++ memcached.c
@@ -54,7 +54,7 @@ std *
 
 /* FreeBSD 4.x doesn't have IOV_MAX exposed. */
 #ifndef IOV_MAX
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) || MAC_OS_X_VERSION_MAX_ALLOWED >= 1030
 # define IOV_MAX 1024
 #endif
 #endif
