5f54ede879939a160168fabbc8cfc462f28e0004
dormando <dormando@rydia.net>
2008-06-29 16:45:59 -0700

    Don't leave stale data on failed set attempt.
    This is mildly less confusing for folks with OOM errors or who store items for long periods of time, but occasionally attempt setting a very large item. While I don't believe this actually fixes a class of bugs we've seen due to refcount leaks, I make this change for consistency.

--- memcached.c
+++ memcached.c
@@ -2365,6 +2365,17 @@ static void process_update_command(conn *c, token_t *tokens, const size_t ntoken
         /* swallow the data line */
         c->write_and_go = conn_swallow;
         c->sbytes = vlen + 2;
+
+        /* Avoid stale data persisting in cache because we failed alloc.
+         * Unacceptable for SET. Anywhere else too? */
+        if (comm == NREAD_SET) {
+            it = item_get(key, nkey);
+            if (it) {
+                item_unlink(it);
+                item_remove(it);
+            }
+        }
+
         return;
     }
     it->cas_id = req_cas_id;
