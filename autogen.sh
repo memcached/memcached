a19b04b18c07ddd71eced7a89f037b760d8e7386
dormando <dormando@rydia.net>
2008-07-24 10:57:09 -0700

    Add support for newer automake's.
    Patch provided by facebook, attribution is to one or more of:
    Marc, Steve, Haiping Zhao, and Paul Saab, Steven Grimm, etc.

--- autogen.sh
+++ autogen.sh
@@ -8,7 +8,7 @@
 #
 
 echo "aclocal..."
-ACLOCAL=`which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15 || which aclocal || exit 1`
+ACLOCAL=`which aclocal-1.10 || which aclocal-1.9 || which aclocal19 || which aclocal-1.7 || which aclocal17 || which aclocal-1.5 || which aclocal15 || which aclocal || exit 1`
 $ACLOCAL || exit 1
 
 echo "autoheader..."
@@ -16,7 +16,7 @@ AUTOHEADER=${AUTOHEADER:-autoheader}
 $AUTOHEADER || exit 1
 
 echo "automake..."
-AUTOMAKE=${AUTOMAKE:-automake-1.7}
+AUTOMAKE=`which automake-1.10 || which automake-1.9 || which automake-1.7 || exit 1`
 $AUTOMAKE --foreign --add-missing || automake --gnu --add-missing || exit 1
 
 echo "autoconf..."
