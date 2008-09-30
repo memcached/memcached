43360802fa26a587f3e0509e47f7728e26f5f646
Toru Maesaka <dev@torum.net>
2008-09-30 15:35:46 +0900

    Fixed indent issues.

--- memcached.c
+++ memcached.c
@@ -3436,12 +3436,12 @@ static int server_socket(const int port, enum protocol prot) {
     snprintf(port_buf, NI_MAXSERV, "%d", port);
     error= getaddrinfo(settings.inter, port_buf, &hints, &ai);
     if (error != 0) {
-      if (error != EAI_SYSTEM)
-        fprintf(stderr, "getaddrinfo(): %s\n", gai_strerror(error));
-      else
-        perror("getaddrinfo()");
+        if (error != EAI_SYSTEM)
+          fprintf(stderr, "getaddrinfo(): %s\n", gai_strerror(error));
+        else
+          perror("getaddrinfo()");
 
-      return 1;
+        return 1;
     }
 
     for (next= ai; next; next= next->ai_next) {
@@ -3470,33 +3470,33 @@ static int server_socket(const int port, enum protocol prot) {
             close(sfd);
             continue;
         } else {
-          success++;
-          if (!IS_UDP(prot) && listen(sfd, 1024) == -1) {
-              perror("listen()");
-              close(sfd);
-              freeaddrinfo(ai);
-              return 1;
-          }
-      }
-
-      if (IS_UDP(prot)) {
-        int c;
-
-        for (c = 1; c < settings.num_threads; c++) {
-            /* this is guaranteed to hit all threads because we round-robin */
-            dispatch_conn_new(sfd, conn_read, EV_READ | EV_PERSIST,
-                              UDP_READ_BUFFER_SIZE, ascii_udp_prot);
+            success++;
+            if (!IS_UDP(prot) && listen(sfd, 1024) == -1) {
+                perror("listen()");
+                close(sfd);
+                freeaddrinfo(ai);
+                return 1;
+            }
         }
-      } else {
-        if (!(listen_conn_add = conn_new(sfd, conn_listening,
-                                      EV_READ | EV_PERSIST, 1,
-                                      prot, main_base))) {
-            fprintf(stderr, "failed to create listening connection\n");
-            exit(EXIT_FAILURE);
+
+        if (IS_UDP(prot)) {
+            int c;
+
+            for (c = 1; c < settings.num_threads; c++) {
+                /* this is guaranteed to hit all threads because we round-robin */
+                dispatch_conn_new(sfd, conn_read, EV_READ | EV_PERSIST,
+                                  UDP_READ_BUFFER_SIZE, ascii_udp_prot);
+            }
+        } else {
+            if (!(listen_conn_add = conn_new(sfd, conn_listening,
+                                             EV_READ | EV_PERSIST, 1,
+                                             prot, main_base))) {
+                fprintf(stderr, "failed to create listening connection\n");
+                exit(EXIT_FAILURE);
+            }
+            listen_conn_add->next = listen_conn;
+            listen_conn = listen_conn_add;
         }
-        listen_conn_add->next = listen_conn;
-        listen_conn = listen_conn_add;
-      }
     }
 
     freeaddrinfo(ai);
@@ -4072,7 +4072,8 @@ int main (int argc, char **argv) {
     /* create unix mode sockets after dropping privileges */
     if (settings.socketpath != NULL) {
         if (server_socket_unix(settings.socketpath,settings.access)) {
-          fprintf(stderr, "failed to listen\n");
+          fprintf(stderr, "failed to listen on UNIX socket: %s\n",
+                  settings.socketpath);
           exit(EXIT_FAILURE);
         }
     }
@@ -4082,7 +4083,7 @@ int main (int argc, char **argv) {
         int udp_port;
 
         if (settings.port && server_socket(settings.port, negotiating_prot)) {
-            fprintf(stderr, "failed to listen\n");
+            fprintf(stderr, "failed to listen on TCP port %d\n", settings.port);
             exit(EXIT_FAILURE);
         }
 
