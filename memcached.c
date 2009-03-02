d2b97e6171e96bd9674d4f1536e2a41c91d8fd19
Evan Klitzke <evan@eklitzke.org>
2009-03-01 17:18:03 -0800

    Use AI_ADDRCONFIG more selectively.
    This makes running on Linux when disconnected possible.
    
    Tests still fail when using the -l option to specify a binding address
    while offline.  This is probably quite unlikely if it weren't for the
    first test.

--- memcached.c
+++ memcached.c
@@ -3536,7 +3536,13 @@ static int server_socket(const int port, enum protocol prot) {
      * that otherwise mess things up.
      */
     memset(&hints, 0, sizeof (hints));
-    hints.ai_flags = AI_PASSIVE|AI_ADDRCONFIG;
+    hints.ai_flags = AI_PASSIVE;
+
+    /* Only use AI_ADDRCONFIG if a hostname is specified, otherwise we might
+     * not get results for INADDR_ANY. */
+    if (settings.inter)
+        hints.ai_flags |= AI_ADDRCONFIG;
+
     if (IS_UDP(prot))
     {
         hints.ai_socktype = SOCK_DGRAM;
