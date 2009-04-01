756cf326e53b0ddbfa1f9d1254851b5881456c6b
dormando <dormando@rydia.net>
2009-04-01 14:20:38 -0700

    'stats reset' resets new main stats.

--- memcached.c
+++ memcached.c
@@ -175,6 +175,7 @@ static void stats_reset(void) {
     STATS_LOCK();
     stats.total_items = stats.total_conns = 0;
     stats.evictions = 0;
+    stats.listen_disabled_num = 0;
     stats_prefix_clear();
     STATS_UNLOCK();
     threadlocal_stats_reset();
