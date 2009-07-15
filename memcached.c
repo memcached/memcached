27bff0d1378e1373a75cd3f954b58e1447a52ca1
Dustin Sallings <dustin@spy.net>
2009-07-14 20:58:39 -0700

    Limit room with snprintf in append_ascii_stats.
    This is part of issue 64.

--- memcached.c
+++ memcached.c
@@ -1276,14 +1276,16 @@ static void append_ascii_stats(const char *key, const uint16_t klen,
                                const char *val, const uint32_t vlen,
                                conn *c) {
     char *pos = c->stats.buffer + c->stats.offset;
-    uint32_t nbytes;
+    uint32_t nbytes = 0;
+    int remaining = c->stats.size - c->stats.offset;
+    int room = remaining - 1;
 
     if (klen == 0 && vlen == 0) {
-        nbytes = sprintf(pos, "END\r\n");
+        nbytes = snprintf(pos, room, "END\r\n");
     } else if (vlen == 0) {
-        nbytes = sprintf(pos, "STAT %s\r\n", key);
+        nbytes = snprintf(pos, room, "STAT %s\r\n", key);
     } else {
-        nbytes = sprintf(pos, "STAT %s %s\r\n", key, val);
+        nbytes = snprintf(pos, room, "STAT %s %s\r\n", key, val);
     }
 
     c->stats.offset += nbytes;
