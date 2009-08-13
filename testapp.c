b8e106f775db38b50602470142136530c20b1125
Dustin Sallings <dustin@spy.net>
2009-08-13 13:53:17 -0700

    The C test should emit TAP.

--- testapp.c
+++ testapp.c
@@ -357,27 +357,23 @@ struct testcase testcases[] = {
 int main(int argc, char **argv)
 {
     int exitcode = 0;
-    int ii;
-    const int paddingsz = 60;
-    char padding[paddingsz + 1];
-    memset(padding, ' ', paddingsz);
+    int ii = 0, num_cases = 0;
+
+    for (num_cases = 0; testcases[num_cases].description; num_cases++) {
+        /* Just counting */
+    }
+
+    printf("1..%d\n", num_cases);
 
     for (ii = 0; testcases[ii].description != NULL; ++ii) {
-        int len = strlen(testcases[ii].description);
-        if (len > paddingsz) {
-            len = paddingsz;
-        }
-        padding[paddingsz - len] = '\0';
-        fprintf(stdout, "%s:%s", testcases[ii].description, padding);
-        padding[paddingsz - len] = ' ';
         fflush(stdout);
         enum test_return ret = testcases[ii].function();
         if (ret == TEST_SKIP) {
-            fprintf(stdout, "[skipped]\n");
+            fprintf(stdout, "ok # SKIP %d - %s\n", ii + 1, testcases[ii].description);
         } else if (ret == TEST_PASS) {
-            fprintf(stdout, "[ok]\n");
+            fprintf(stdout, "ok %d - %s\n", ii + 1, testcases[ii].description);
         } else {
-            fprintf(stdout, "[failed]\n");
+            fprintf(stdout, "not ok %d - %s\n", ii + 1, testcases[ii].description);
             exitcode = 1;
         }
         fflush(stdout);
