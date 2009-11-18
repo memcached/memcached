de682e0c65126fb945cea57447f49e69f0fa72ef
Trond Norbye <Trond.Norbye@sun.com>
2009-11-18 11:41:51 +0100

    Fixed printf formatting spefifier

--- slabs.c
+++ slabs.c
@@ -313,8 +313,8 @@ bool get_stats(const char *stat_type, int nkey, ADD_STAT add_stats, void *c) {
             /* prepare general statistics for the engine */
             pthread_mutex_lock(&default_engine.stats.lock);
             APPEND_STAT("bytes", "%"PRIu64, default_engine.stats.curr_bytes);
-            APPEND_STAT("curr_items", "%u", default_engine.stats.curr_items);
-            APPEND_STAT("total_items", "%u", default_engine.stats.total_items);
+            APPEND_STAT("curr_items", "%u"PRIu64, default_engine.stats.curr_items);
+            APPEND_STAT("total_items", "%u"PRIu64, default_engine.stats.total_items);
             APPEND_STAT("evictions", "%"PRIu64, default_engine.stats.evictions);
             APPEND_STAT("reclaimed", "%"PRIu64, default_engine.stats.reclaimed);
             pthread_mutex_unlock(&default_engine.stats.lock);
