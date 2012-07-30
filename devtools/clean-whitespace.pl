67c90606ee65b0e4606a4a76ace2f56d618220f6
dormando <dormando@rydia.net>
2012-07-29 22:21:05 -0700

    whitespace fix for freebsd
    I dunno why it litters .orgs then tries to run them. I'm not a magician.

--- devtools/clean-whitespace.pl
+++ devtools/clean-whitespace.pl
@@ -3,7 +3,7 @@ use strict;
 use FindBin qw($Bin);
 chdir "$Bin/.." or die;
 
-my @exempted = qw(Makefile.am ChangeLog doc/Makefile.am);
+my @exempted = qw(Makefile.am ChangeLog doc/Makefile.am README README.md);
 push(@exempted, glob("doc/*.xml"));
 push(@exempted, glob("doc/xml2rfc/*.xsl"));
 push(@exempted, glob("m4/*backport*m4"));
--- t/whitespace.t
+++ t/whitespace.t
@@ -10,6 +10,7 @@ BEGIN {
     push(@exempted, glob("doc/*.xml"));
     push(@exempted, glob("doc/xml2rfc/*.xsl"));
     push(@exempted, glob("m4/*backport*m4"));
+    push(@exempted, glob("*.orig"));
     my %exempted_hash = map { $_ => 1 } @exempted;
 
     my @stuff = split /\0/, `git ls-files -z -c -m -o --exclude-standard`;
