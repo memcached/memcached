88b08e7ef75b3d7a24e1c79f201e56cdd93e6b0f
Alex Leone <acleone@gmail.com>
2013-12-18 20:34:24 -0800

    Fix udp mode when listening on ipv6 addresses.
    Previously the memcached could not respond to ipv6 udp packets correctly,
    since the client's address was stored in a struct sockaddr (16 bytes on
    linux) instead of a struct sockaddr_in6 (28 bytes).

--- memcached.h
+++ memcached.h
@@ -436,7 +436,7 @@ struct conn {
 
     /* data for UDP clients */
     int    request_id; /* Incoming UDP request ID, if this is a UDP "connection" */
-    struct sockaddr request_addr; /* Who sent the most recent request */
+    struct sockaddr_in6 request_addr; /* udp: Who sent the most recent request */
     socklen_t request_addr_size;
     unsigned char *hdrbuf; /* udp packet headers */
     int    hdrsize;   /* number of headers' worth of space is allocated */
