846a3c3916bc8eaef77c0cb0028f79f732d27ad7
Steve Wills <steve@mouf.net>
2015-01-16 16:58:18 +0000

    fix build with clang 3.5

--- thread.c
+++ thread.c
@@ -401,7 +401,7 @@ static void thread_libevent_process(int fd, short which, void *arg) {
         conn *c = conn_new(item->sfd, item->init_state, item->event_flags,
                            item->read_buffer_size, item->transport, me->base);
         if (c == NULL) {
-            if (IS_UDP(item->transport)) {
+            if IS_UDP(item->transport) {
                 fprintf(stderr, "Can't listen for events on UDP socket\n");
                 exit(1);
             } else {
