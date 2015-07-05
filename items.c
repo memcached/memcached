cbb9ec8c66d216aaa9db808d1e4673b1055a8b02
dormando <dormando@rydia.net>
2015-07-04 18:11:26 -0700

    remove another invalid assert()
    slab class 255 is now valid, and it's a uint8_t

--- items.c
+++ items.c
@@ -1138,7 +1138,6 @@ static item *crawler_crawl_q(item *it) {
     item **head, **tail;
     assert(it->it_flags == 1);
     assert(it->nbytes == 0);
-    assert(it->slabs_clsid < LARGEST_ID);
     head = &heads[it->slabs_clsid];
     tail = &tails[it->slabs_clsid];
 
