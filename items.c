bafd13d33cdc844c15d69f07d19428cd97946576
dormando <dormando@rydia.net>
2017-01-01 23:53:17 -0800

    If hit in COLD, immediately move to WARM
    Ignores the wait period.

--- items.c
+++ items.c
@@ -443,24 +443,30 @@ void do_item_update_nolock(item *it) {
 /* Bump the last accessed time, or relink if we're in compat mode */
 void do_item_update(item *it) {
     MEMCACHED_ITEM_UPDATE(ITEM_key(it), it->nkey, it->nbytes);
-    if (it->time < current_time - ITEM_UPDATE_INTERVAL) {
+
+    /* Hits to COLD_LRU immediately move to WARM. */
+    if (settings.lru_maintainer_thread) {
+        assert((it->it_flags & ITEM_SLABBED) == 0);
+        if ((it->it_flags & ITEM_LINKED) != 0) {
+            // FIXME: do better LRU check.
+            if (it->slabs_clsid >= COLD_LRU && it->slabs_clsid < NOEXP_LRU) {
+                it->time = current_time;
+                item_unlink_q(it);
+                it->slabs_clsid = ITEM_clsid(it);
+                it->slabs_clsid |= WARM_LRU;
+                it->it_flags &= ~ITEM_ACTIVE;
+                item_link_q_warm(it);
+            } else if (it->time < current_time - ITEM_UPDATE_INTERVAL) {
+                it->time = current_time;
+            }
+        }
+    } else if (it->time < current_time - ITEM_UPDATE_INTERVAL) {
         assert((it->it_flags & ITEM_SLABBED) == 0);
 
         if ((it->it_flags & ITEM_LINKED) != 0) {
             it->time = current_time;
-            // FIXME: do better LRU check.
-            if (settings.lru_maintainer_thread) {
-                if (it->slabs_clsid >= COLD_LRU && it->slabs_clsid < NOEXP_LRU) {
-                    item_unlink_q(it);
-                    it->slabs_clsid = ITEM_clsid(it);
-                    it->slabs_clsid |= WARM_LRU;
-                    it->it_flags &= ~ITEM_ACTIVE;
-                    item_link_q_warm(it);
-                }
-            } else {
-                item_unlink_q(it);
-                item_link_q(it);
-            }
+            item_unlink_q(it);
+            item_link_q(it);
         }
     }
 }
