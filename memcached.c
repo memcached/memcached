c96fd708b2e262376a97ffdb79209f747fd0bd75
dormando <dormando@rydia.net>
2017-05-21 15:40:58 -0700

    usability fix for cache_memlimit
    throws an error if someone asks for a petabyte of RAM (I can't wait for the
    day this bites me). specify the unit properly in documentation.
    
    thanks to @arodland for the report.

--- memcached.c
+++ memcached.c
@@ -3880,7 +3880,9 @@ static void process_memlimit_command(conn *c, token_t *tokens, const size_t ntok
         if (memlimit < 8) {
             out_string(c, "MEMLIMIT_TOO_SMALL cannot set maxbytes to less than 8m");
         } else {
-            if (slabs_adjust_mem_limit((size_t) memlimit * 1024 * 1024)) {
+            if (memlimit > 1000000000) {
+                out_string(c, "MEMLIMIT_ADJUST_FAILED input value is megabytes not bytes");
+            } else if (slabs_adjust_mem_limit((size_t) memlimit * 1024 * 1024)) {
                 if (settings.verbose > 0) {
                     fprintf(stderr, "maxbytes adjusted to %llum\n", (unsigned long long)memlimit);
                 }
