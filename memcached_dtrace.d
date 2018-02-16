0348682d54bf7660b0a4d4551cecddda6a159334
dormando <dormando@rydia.net>
2018-02-15 22:19:00 -0800

    fix for dtrace compilation

--- memcached_dtrace.d
+++ memcached_dtrace.d
@@ -111,19 +111,15 @@ provider memcached {
     * Fired when a new item has been inserted.
     * @param key the key just inserted
     * @param keylen length of the key
-    * @param nokeys the total number of keys currently stored,
-    *               including the key for which insert was called.
     */
-   probe assoc__insert(const char *key, int keylen, int nokeys);
+   probe assoc__insert(const char *key, int keylen);
 
    /**
     * Fired when a new item has been removed.
     * @param key the key just deleted
     * @param keylen length of the key
-    * @param nokeys the total number of keys currently stored,
-    *               excluding the key for which delete was called.
     */
-   probe assoc__delete(const char *key, int keylen, int nokeys);
+   probe assoc__delete(const char *key, int keylen);
 
    /**
     * Fired when an item is linked into the cache.
