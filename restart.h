893773ecaae32cf95cd1038187ac02f9dd78d327
minkikim89 <minkikim89@gmail.com>
2019-10-21 02:10:18 +0200

    restart: remove unused restart_mmap_set function

--- restart.h
+++ restart.h
@@ -23,7 +23,6 @@ enum restart_get_kv_ret restart_get_kv(void *ctx, char **key, char **val);
 
 bool restart_mmap_open(const size_t limit, const char *file, void **mem_base);
 void restart_mmap_close(void);
-void restart_mmap_set(void);
 unsigned int restart_fixup(void *old_base);
 
 #endif
