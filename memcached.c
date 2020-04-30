cefdf1571a6f6b9b93dac87e7da909559eb90db4
dormando <dormando@rydia.net>
2020-04-30 14:04:28 -0700

    minimum libevent version is 2.x
    1.4 is crazy old now :(

--- memcached.c
+++ memcached.c
@@ -8381,13 +8381,9 @@ static bool sanitycheck(void) {
     const char *ever = event_get_version();
     if (ever != NULL) {
         if (strncmp(ever, "1.", 2) == 0) {
-            /* Require at least 1.3 (that's still a couple of years old) */
-            if (('0' <= ever[2] && ever[2] < '3') && !isdigit(ever[3])) {
-                fprintf(stderr, "You are using libevent %s.\nPlease upgrade to"
-                        " a more recent version (1.3 or newer)\n",
-                        event_get_version());
-                return false;
-            }
+            fprintf(stderr, "You are using libevent %s.\nPlease upgrade to 2.x"
+                        " or newer\n", event_get_version());
+            return false;
         }
     }
 
