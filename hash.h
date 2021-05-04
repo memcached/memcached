1aa879fe023e0340755b6b9a8935dfc1f1fae462
dormando <dormando@rydia.net>
2021-05-03 19:08:02 -0700

    hash: add XXH3 to list of hash algorithms.
    needs more testing. in a "pure miss" test to help show hashing overhead,
    with four worker threads and large ascii multigets there is a measurable
    performance boost for longer keys. Very short keys (10-15 bytes) are
    about even.
    
    for 150ish byte keys 12m rps -> 13m rps roughly. Not too bad!
    
    need to test hash table distribution for large sets of keys before
    merging. need to consider if worth switching the defaults now or later.

--- hash.h
+++ hash.h
@@ -5,7 +5,7 @@ typedef uint32_t (*hash_func)(const void *key, size_t length);
 extern hash_func hash;
 
 enum hashfunc_type {
-    JENKINS_HASH=0, MURMUR3_HASH
+    JENKINS_HASH=0, MURMUR3_HASH, XXH3_HASH
 };
 
 int hash_init(enum hashfunc_type type);
