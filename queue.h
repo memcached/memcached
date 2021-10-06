7e570c789f4473354461e6eeb8bb7a283df613bf
dormando <dormando@rydia.net>
2021-10-06 13:34:12 -0700

    core: remove cdefs include from queue.h
    compatibility issue - seems to only exist to ensure NULL is defined
    properly; but at the point in which we include the queue.h, NULL most
    certainly exists.

--- queue.h
+++ queue.h
@@ -37,8 +37,6 @@
 #ifndef _SYS_QUEUE_H_
 #define	_SYS_QUEUE_H_
 
-#include <sys/cdefs.h>
-
 /*
  * This file defines four types of data structures: singly-linked lists,
  * singly-linked tail queues, lists and tail queues.
