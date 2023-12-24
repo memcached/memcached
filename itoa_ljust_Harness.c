31eb48ae3917c23e7131a325db40a49bab03c478
Rigon <ajdinirig@gmail.com>
2023-12-24 13:46:33 +0100

    itoa_ljust final experiment
    str length is a char so only length of 255, no abort() or exit(). -> output_ilj_finalExp

--- itoa_ljust_Harness.c
+++ itoa_ljust_Harness.c
@@ -63,50 +63,16 @@ char* itoa_u32(uint32_t u, char* p) {
     return itoa( u, p, d, n );
 }
 
-int generateRandomNumber(int seed, int min, int max) {
-    // Set the seed for the random number generator
-    srand(seed);
-
-    // Generate and return a random number within the specified range
-    return min + rand() % (max - min + 1);
-}
-
-//for char* p we need a __VERIFIER_nondet_String which we build here from scratch
-//char* verifier_nondet_string() {
-//    int seed = __VERIFIER_nondet_int();
-//    char s = __VERIFIER_nondet_char();
-//    // int length = __VERIFIER_nondet_int() needs to be limited,
-//    // because the fuzzer needs way to long for very high lengths,
-//    // so we just use random_function with a limit set by me
-//    int length = generateRandomNumber(seed, 1, 1000);
-//
-//    // Allocate memory for the string
-//    char* str = (char*)malloc(length + 1);
-//
-//    for(int i = 0; i < length; i++){
-//        str[i] = s;
-//    }
-//
-//    // Null-terminate the string
-//    str[length] = '\0';
-//
-//    free(str);
-//    return str;
-//}
-
 int main() {
-    //char* p = verifier_nondet_string();
     uint32_t u = __VERIFIER_nondet_uint();
 
-    unsigned int strlength = __VERIFIER_nondet_uint();
-    if(strlength > 1000) {
-        abort();
-    }
+    unsigned char strlength = __VERIFIER_nondet_char();
+
     char* str = (char*)malloc(strlength + 1);
     for(int i = 0; i < strlength; i++){
         str[i] = __VERIFIER_nondet_char();
     }
-    str[strlength-1] = '\0';
+    str[strlength] = '\0';
 
     printf("uint32_t: %u, String: %s\n", u, str);
 
