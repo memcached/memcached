601b1bcda51b51780dde2022a8a26a620577251a
yeoncheol-kim <koo05131@yahoo.co.kr>
2024-11-08 15:38:31 +0900

    FIX: Do conn_release_items when a TRANSMIT_HARD_ERROR occurs in the UDP protocol

--- memcached.c
+++ memcached.c
@@ -2903,6 +2903,7 @@ static enum transmit_result transmit_udp(conn *c) {
     if (settings.verbose > 0)
         perror("Failed to write, and not due to blocking");
 
+    conn_release_items(c);
     conn_set_state(c, conn_read);
     return TRANSMIT_HARD_ERROR;
 }
